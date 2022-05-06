fc0f0bd3e9bab99dee33ba0c6a2766002d15f7ef
delissgt <delissgt@gmail.com>
2022-05-05 21:03:00 -0500

    Deploy with configuracion surge with circle ci

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>Deliss was here!</p>
+        <p>deploy with configuration surge</p>
         <a
           className="App-link"
           href="https://reactjs.org"
